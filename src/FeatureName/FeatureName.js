0df62faf5c874c785246ef9216aeb527f616a62e
mertbagt <73559781+mertbagt@users.noreply.github.com>
2020-12-02 17:40:52 -0500

    removed unneeded slugify

--- src/FeatureName/FeatureName.js
+++ src/FeatureName/FeatureName.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from 'slugify';
 import './FeatureName.css';
 
 class FeatureName extends Component {
